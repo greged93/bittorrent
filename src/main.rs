eb8c0c2c3b0f5d89d96015ed2cbfbda2f4a77019
Gregory Edison <gregory.edison1993@gmail.com>
2024-09-14 09:47:30 +0200

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-use serde_json;
-use std::env;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+pub struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+pub enum Command {
+    Decode { input: String },
+}
 
 // Available if you need it!
 // use serde_bencode
@@ -21,18 +31,15 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command = &args[1];
+    let command = Cli::parse();
+    match command.command {
+        Command::Decode { input } => {
+            // You can use print statements as follows for debugging, they'll be visible when running tests.
+            println!("Logs from your program will appear here!");
 
-    if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
-    } else {
-        println!("unknown command: {}", args[1])
+            // Uncomment this block to pass the first stage
+            let decoded_value = decode_bencoded_value(&input);
+            println!("{}", decoded_value);
+        }
     }
 }
